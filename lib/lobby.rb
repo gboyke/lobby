aab043dc6555e84f11d6db977ec5f6dc8c887b5a
gboyke <guido.boyke@gboyke.com>
2013-12-22 23:57:34 +0000

    load paths

--- lib/lobby.rb
+++ lib/lobby.rb
@@ -1,7 +1,12 @@
 module Lobby
   require "lobby/engine"
-  require "../../app/models/lobby"
-  require "../../app/mailers/lobby"
-  require "../../app/forms/lobby"
-  require "../../app/controllers/lobby"
+  require "../../app/models/lobby/authentication"
+  require "../../app/models/lobby/auth_user"
+  require "../../app/models/lobby/password_forgotten_form_abstract"
+  require "../../app/mailers/lobby/confirmation_mailer"
+  require "../../app/forms/lobby/password_forgotten_form"
+  require "../../app/controllers/lobby/confirmation_controller"
+  require "../../app/controllers/lobby/password_forgotten_controller"
+  require "../../app/controllers/lobby/sessions_controller"
+  require "../../app/controllers/lobby/users_controller"
 end
--- lib/lobby/version.rb
+++ lib/lobby/version.rb
@@ -1,3 +1,3 @@
 module Lobby
-  VERSION = "0.0.7"
+  VERSION = "0.0.8"
 end
